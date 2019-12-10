5d3c5e0454c084da54188293950ddf5532657e00
Viviane P <vvne6@yahoo.com.br>
2019-12-10 19:09:49 -0300

    corrigido

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -6,14 +6,13 @@ public class Runner {
 		CorreioStep cStp= new CorreioStep(); 
 		System.out.println("------------TESTES CORREIOS----------------------------------\n");
 		
-		/*
 		//Teste 1: No site dos correios, pesquisar um CEP e dar output das informações retornadas.
 		System.out.println("Cenário: Verificar no site dos correios se busca por CEP retorna informações corretas.");
 		cStp.AcessarBuscaCep("http://www.buscacep.correios.com.br/sistemas/buscacep/buscaCepEndereco.cfm", "Endereço por CEP");
 		cStp.PreencherDadosCep();
 		cStp.validaResultado();
 		cStp.fechaPagina();
-		*/
+		
 		System.out.println("\n\n\n");
 
 		//Teste 2: No site dos correios, pesquisar um logradouro que retorne mais de um resultado e dar output de todos os dados da tabela retornada.
